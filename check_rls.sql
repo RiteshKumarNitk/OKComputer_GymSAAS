c509de8370e4d2f0a4b544dc3aed2f53740f8adc
Ritesh Kumar Nitk <Riteshkumar.nitk21@gmail.com>
2026-03-17 14:11:04 +0530

    removed unused files

--- check_rls.sql
+++
@@ -1,2 +0,0 @@
--- check_rls.sql
-SELECT * FROM pg_policies WHERE tablename = 'tenants';
--- confirm_user.sql
+++
@@ -1,7 +0,0 @@
--- Manually confirm a user's email address
--- Run this script to bypass email verification for a specific user.
-
--- Replace 'YOUR_EMAIL_HERE' with the email address you want to confirm.
-UPDATE auth.users
-SET email_confirmed_at = NOW()
-WHERE email = 'riteshkumar.nitk21@gmail.com'; -- Example email, change this!
